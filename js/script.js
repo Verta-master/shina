db4ee02f950233081d73f038d7cf254ff9ba495c
Verta-master <vertyagina@gmail.com>
2021-01-21 17:24:55 +0600

    Update 2021-01-21T11:24:51.446Z

--- js/script.js
+++ js/script.js
@@ -8,19 +8,22 @@ $('.header__close').click(function() {
   $('.header__drop').slideUp();
 })
 
+//Header scroll
+if (window.innerWidth > 1200) {
+  $(window).scroll(function(){
+    if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+      $('header').addClass('header--scroll');
+    } else {
+      $('header').removeClass('header--scroll');
+    };
+  })
+} else {
+  $('header').addClass('header--mobile');
+}
 
-//if (window.innerWidth < 766) {
-//  $('.menu__link').click(function() {
-//    $('.menu__list').slideToggle();
-//    $('.menu').removeClass('menu--opened');
-//  })
-//}
-
-//Scroll to top
-$(window).scroll(function(){
-  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
-    $('header').addClass('header--scroll');
-  } else {
-    $('header').removeClass('header--scroll');
-  };
-});
+//Search
+if (window.innerWidth < 767) {
+  $('.search__open').click(function() {
+    $('.search__form').toggleClass('search__form--show');
+  })
+}
